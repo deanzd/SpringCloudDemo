c7dd437720598f2df4be1df2f39cddf27c19183f
Dean <308645550@qq.com>
2018-07-04 21:56:44 +0800

    Initial commit

---
+++ eureka-client/src/main/java/com/dean/demo/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,28 @@
+package com.dean.demo.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/client1")
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/test")
+    public String test(@RequestParam("name") String name) {
+        return name + ", my port is " + port;
+    }
+}
